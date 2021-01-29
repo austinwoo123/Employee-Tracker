fbdefe02bcb5c970f445127fbd34041dbddb8fcd
Austin W <austinwoo123@gmail.com>
2021-01-28 20:49:52 -0800

    finish creating tables and start on seed sequel file

--- schema.sql
+++ schema.sql
@@ -5,6 +5,28 @@ CREATE DATABASE employeTracker_DB;
 USE employeTracker_DB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCEREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30)NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    manager_id INT,
+    role_id INT,
+    Primary KEY (id),
+
+);
+
---
+++ seed.sql
@@ -0,0 +1,4 @@
+USE employeTracker_DB
+
+INSERT INTO department (name)
+VALUE ("Sales"),("Engineering"),("Finance"), ("Legal")
